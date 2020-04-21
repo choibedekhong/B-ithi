0807ba545014cfc100b35bcb8da1f3a6199aabcd
Nguyen Truong Khang <khangnguyen3161@gmail.com>
2020-04-21 11:49:18 +0700

    lam lai

--- SoManhMe.java
+++ SoManhMe.java
@@ -12,37 +12,18 @@ public class SoManhMe {
 		return true;
 	}
 
-	public static void main(String[] args) {
-		System.out.println("Xac Dinh So Manh Me");
-		Scanner scan = new Scanner(System.in);
-		int j = 0;
-		while (j < 100) {
-			System.out.print("Vui long nhap so: ");
-			int number = scan.nextInt();
-			boolean ketqua = false;	
-		   
-			for (int i = 2; i <= Math.sqrt(number); i++) {
-				if (soNguyenTo(i) == true) {
-					if (number % i == 0) {
-					if (number % Math.pow(i,2) == 0) {
-						ketqua = true;
-						break;
+	public static boolean soManhMe(int number) {
+		for (int i = 2 ; i <= Math.sqrt(number); i++) {
+
+			if (soNguyenTo(i) == true) {
+				if (number % i == 0) {
+					if (number % Math.pow(i, 2) == 0) {
+						return true;
 					}
-					}
-					
-					else {
-						ketqua = false;
-					}
-				
-			}
-			}
-		  
-			if (ketqua == true) {
-				System.out.println(  number +" la so manh me");
-			} 
-			else {
-				System.out.println(  number +" khong la so manh me" );
+				}
 			}
 		}
+		return false;
+
 	}
 }
